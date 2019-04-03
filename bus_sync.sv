cf0a92498e546b65dc60f331da3230b9b10b3317
Mat Hildebrand <mathewhildebrand@gmail.com>
2019-04-03 00:39:28 -0400

    2nd commit

---
+++ bus_sync.sv
@@ -0,0 +1,42 @@
+`default_nettype none
+module  bus_sync 
+    #(
+    parameter BUS_WIDTH  =1,
+    parameter NUM_RETIME =2   
+    )
+    
+    (
+    input   logic                i_clk_b,
+    input   logic [BUS_WIDTH-1:0] i_data_a,
+    output  logic [BUS_WIDTH-1:0] o_data_b
+    
+);
+
+
+
+   logic [BUS_WIDTH-1:0]   sync_reg1;
+   logic [BUS_WIDTH-1:0]   sync_reg2;
+   
+   
+cov_core_bits_sync #(
+	.BUS_WIDTH   (BUS_WIDTH  ), 
+	.NUM_RETIME  (NUM_RETIME )
+	) cov_core_bits_sync (
+	.i_clk_b     (i_clk_b    ), 
+	.i_data_a    (i_data_a   ), 
+	.o_data_b    (sync_reg1  ));
+	
+always @(posedge i_clk_b) begin
+	sync_reg2 <= sync_reg1;
+end
+
+always @(posedge i_clk_b) begin
+	if (sync_reg2 == sync_reg1)
+		o_data_b <= sync_reg2;
+end
+	
+	
+endmodule
+
+`default_nettype wire	
+	
